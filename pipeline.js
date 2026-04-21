00a70e02a7c0dd8902829fe8de47c49e82a5a8e7
alwayssomewhattired <zacharymalinka@gmail.com>
2026-04-20 21:08:39 -0400

    Got triangle to work with buffered vertices

--- pipeline.js
+++ pipeline.js
@@ -2,7 +2,8 @@
 import { getDevice } from "./webgpu.js"
 import { getShaderModule} from './webgpu.js'
 
-let pipeline = null;
+let m_pipeline = null;
+let m_positionBuffer = null;
 
 export function initPipeline() {
     const device = getDevice();
@@ -15,12 +16,39 @@ export function initPipeline() {
         format: 'bgra8unorm'
     };
 
+    const positionAttribDesc = {
+        shaderLocation: 0, // location(0)
+        offset: 0,
+        format: 'float32x3'
+    };
+
+    const positionLayoutBufferDesc = {
+        attributes: [positionAttribDesc],
+        arrayStride: 4 * 3, // sizeof float * 3
+        stepMode: 'vertex'
+    };
+
+    const positions = new Float32Array([
+        1.0, -1.0, 0.0, -1.0, -1.0, 0.0, 0.0, 1.0, 0.0
+    ]);
+
+    const positionBufferDesc = {
+        size: positions.byteLength,
+        usage: GPUBufferUsage.VERTEX,
+        mappedAtCreation: true
+    };
+
+    m_positionBuffer = device.createBuffer(positionBufferDesc);
+    const writeArray = new Float32Array(m_positionBuffer.getMappedRange());
+    writeArray.set(positions);
+    m_positionBuffer.unmap();
+
     const pipelineDesc = {
         layout,
         vertex: {
             module: shaderModule,
             entryPoint: 'vs_main',
-            buffers: []
+            buffers: [positionLayoutBufferDesc]
         },
         fragment: {
             module: shaderModule,
@@ -29,18 +57,26 @@ export function initPipeline() {
         },
         primitive: {
             topology: 'triangle-list',
-            frontFace: 'ccw',
+            frontFace: 'cw',
             cullMode: 'back'
         }
     };
-
-    pipeline = device.createRenderPipeline(pipelineDesc);
+   
+    m_pipeline = device.createRenderPipeline(pipelineDesc);
 }
 
 export function getPipeline() {
-    if (!pipeline) {
+    if (!m_pipeline) {
         throw new Error("Pipeline is not initialized!");
     }
 
-    return pipeline;
+    return m_pipeline;
+}
+
+export function getPositionBuffer() {
+    if (!m_positionBuffer) {
+        throw new Error("Position buffer is unavailable!");
+    }
+
+    return m_positionBuffer;
 }
